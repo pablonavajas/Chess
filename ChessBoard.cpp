aff241bcecaf546fc69ebb35d82bfcc3e9184d30
Pablo Navajas Helguero <pn319@corona56.doc.ic.ac.uk>
2019-12-09 23:34:23 +0000

    spacing trial#2

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -73,7 +73,7 @@ void ChessBoard::resetBoard() {
     team = Black;
   }
 
-  std::cout << "A new Chess Game is started!" << std::endl << std::endl;
+  std::cout << "A new Chess Game is started!\n";
 }
 
 bool ChessBoard::checkCoord(string position) {
